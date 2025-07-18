02776cd3131ab851242850c583911c6b2a5ad3bf
Carl-Erik Kopseng <carlerik@gmail.com>
2025-07-18 08:36:18 +0200

    Remove unused keepalive script

--- keep-alive.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# USAGE: this script is supposed to be put in your crontab. 
-NAME=minecraft-monitor.scpt
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if ! pgrep -f "osascript.*$NAME" >/dev/null; then
-    osascript "$SCRIPT_DIR/$NAME" "$SCRIPT_DIR/config.plist" &
-fi
